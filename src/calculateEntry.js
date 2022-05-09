98a25154419f69327582425d83a61fef348b586f
GabrielF <ogabrielfef@gmail.com>
2022-05-09 18:03:26 -0300

    questao6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const entries = entrants.reduce((acc, entrant) => {
+    if (entrant.age < 18) {
+      acc.child += 1;
+      return acc;
+    }
+    if (entrant.age >= 18 && entrant.age < 50) {
+      acc.adult += 1;
+      return acc;
+    }
+    if (entrant.age >= 50) {
+      acc.senior += 1;
+      return acc;
+    }
+  }, { adult: 0, child: 0, senior: 0 });
+  return entries;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) return 0;
+  if (!entrants.length) return 0;
+  return entrants.adult * prices.adult + entrants.child * prices.child
+   + entrants.senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
