67bb344e55e3dd95c5a4e7b4f45851d54f1adcb3
GabrielF <ogabrielfef@gmail.com>
2022-05-09 16:51:55 -0300

    questao3 dnv

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  if (!employeeName) return {};
+  if (!employeeName) {
+    return {};
+  }
   const objEmploy = data.employees.find((value) => value.firstName === employeeName.firstName
   || value.lastName === employeeName);
   return objEmploy;
