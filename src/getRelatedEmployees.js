ff14b4661f1d2bbcbd28acaf3dde408c9c9a9481
GabrielF <ogabrielfef@gmail.com>
2022-05-09 16:32:28 -0300

    questao4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+function isManager(id){ 
+ return data.employees.some((value) => value.managers.includes(id));
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const manager = data.employees.filter((value) => value.managers.includes(managerId));
+    const imprimeNomes = manager.map((value) => `${value.firstName} ${value.lastName}`);
+    return imprimeNomes;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
