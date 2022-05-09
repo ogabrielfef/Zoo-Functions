a80fba1554af84af555eb467c3e5abde0c32561f
GabrielF <ogabrielfef@gmail.com>
2022-05-09 17:13:32 -0300

    questao5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const spec = species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+    return spec;
+  }
+  const speciesFind = species.find(((value) => animal.specie.includes(value.name)));
+  const speciesSexFilter = speciesFind.residents.filter((value) => animal.sex === value.sex);
+  if (!animal.sex) {
+    return speciesFind.residents.length;
+  }
+  return speciesSexFilter.length;
 }
 
 module.exports = countAnimals;
