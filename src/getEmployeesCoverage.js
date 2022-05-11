359f917e56f9becc20b0c8db1fa49106e6f73766
GabrielF <ogabrielfef@gmail.com>
2022-05-11 18:10:35 -0300

    req 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployerDatas(employe) {
+  const responsavel = data.species
+    .filter((resident) => employe.responsibleFor.includes(resident.id))
+    .map((re) => re.name);
+  const responsavelLo = data.species
+    .filter((resident) => employe.responsibleFor.includes(resident.id))
+    .map((re) => re.location);
+  return {
+    id: employe.id,
+    fullName: `${employe.firstName} ${employe.lastName}`,
+    species: responsavel,
+    locations: responsavelLo,
+  };
+}
+
+function getEmployeesCoverage(id) {
+  if (id) {
+    const encarregado = data.employees.find(
+      (val) => val.firstName === id.name || val.lastName === id.name || val.id === id.id,
+    );
+    if (encarregado) { return getEmployerDatas(encarregado); }
+    throw new Error('Informações inválidas');
+  }
+  return data.employees.map((encarregado) => getEmployerDatas(encarregado));
 }
 
 module.exports = getEmployeesCoverage;
