554336e2083f8df9f523c96d282346720310de2f
GabrielF <ogabrielfef@gmail.com>
2022-05-11 15:01:32 -0300

    req 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -6,8 +6,8 @@ function getOldestFromFirstSpecies(id) {
   const especie = data.species.find((specie) => specie.id === colaboradores.responsibleFor[0]);
   const validaIdade = especie.residents.map((object) =>
     object.age).sort((age1, age2) => (age2 - age1))[0];
-  const animalMaisVelho = Object.values(especie.residents.find((animal) => animal.age === validaIdade));
-  return animalMaisVelho;
+  const MaisVelho = Object.values(especie.residents.find((animal) => animal.age === validaIdade));
+  return MaisVelho;
 }
 
 module.exports = getOldestFromFirstSpecies;
