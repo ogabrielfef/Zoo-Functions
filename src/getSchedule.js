4cc5fd6fc72e472e5817bd1b33b0455e5d6aaa66
GabrielF <ogabrielfef@gmail.com>
2022-05-11 19:01:31 -0300

    req 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const { species } = data;
+const { hours } = data;
+
+function disp(day) {
+  if (day === 'Monday') return 'The zoo will be closed!';
+  const animais = species.filter((animal) =>
+    animal.availability.includes(day)).map((animal) => animal.name);
+  return animais;
 }
 
+function opened(day, schedule) {
+  if (day === 'Monday') return 'CLOSED';
+  return `Open from ${schedule.open}am until ${schedule.close}pm`;
+}
+
+function getSchedule(scheduleTarget) {
+  const dispDay = species.find((especie) => scheduleTarget === especie.name);
+  if (dispDay) return dispDay.availability;
+  const schedule = {};
+  Object.entries(hours).filter(([day, value]) =>
+    !scheduleTarget || !Object.keys(hours).includes(scheduleTarget) || scheduleTarget === day)
+    .forEach(([day, value]) => {
+      schedule[day] = {
+        officeHour: opened(day, value),
+        exhibition: disp(day),
+      };
+    });
+  return schedule;
+}
+
+// console.log(getSchedule('lions'));
 module.exports = getSchedule;
